3c9b501b5d1c03141d6a4bc7b7270500d9818b60
Gabriel Kauã <gabrielkborgessilva@gmail.com>
2025-09-16 23:11:29 -0300

    Adicionados atributos e classe interna No em ListaDuplamenteEncadeada

--- ListaDuplamenteEncadeada.java
+++ ListaDuplamenteEncadeada.java
@@ -1,3 +1,10 @@
 public class ListaDuplamenteEncadeada{
-  
+   private static class No{
+        Processo val;
+        No ant, prox;
+        No(Processo p){this.val = p; }
+    }
+    private No ini;
+    private No fim;
+    private int size;
 }
