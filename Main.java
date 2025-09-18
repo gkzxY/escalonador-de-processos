c3294fc5339d8469389bb96ec52cc0dc36b46be8
ksouvu <canutoisael@gmail.com>
2025-09-17 22:01:51 -0300

    verificação do arquivo como argumento

--- Main.java
+++ Main.java
@@ -2,4 +2,7 @@ import java.util.Scanner;
 
 public class main{
   public static void main(String[] args){
-    
+     if (args.length < 1) {
+            System.err.println("Uso: java Main <caminho-arquivo.txt|.csv>");
+            return;
+        }
