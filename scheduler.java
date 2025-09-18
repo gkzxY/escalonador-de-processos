b7c70d353dbeaee39e856c033280524322256629
Gabriel Kauã <gabrielkborgessilva@gmail.com>
2025-09-17 21:29:45 -0300

    Cria classe Scheduler com atributos e filas de processos

--- scheduler.java
+++ scheduler.java
@@ -1,1 +1,9 @@
+public class Scheduler {
+    private ListaDuplamenteEncadeada alta = new ListaDuplamenteEncadeada();
+    private ListaDuplamenteEncadeada media = new ListaDuplamenteEncadeada();
+    private ListaDuplamenteEncadeada baixa = new ListaDuplamenteEncadeada();
+    private ListaDuplamenteEncadeada bloqueados = new ListaDuplamenteEncadeada();
 
+    private int ciclo = 0;
+    private int contAltaSeguidas = 0;
+}
